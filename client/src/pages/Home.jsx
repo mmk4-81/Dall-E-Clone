742cf5cc551074c65bcc0ed0e390949b0708fca7
mmk4-81 <mmkkarbalaei@gmail.com>
2025-02-13 09:47:00 +0330

    Add fetchPosts function to home page to retrieve and display posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,31 @@ const Home = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
+  const fetchPosts = async () => {
+    setLoading(true);
 
+    try {
+      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
