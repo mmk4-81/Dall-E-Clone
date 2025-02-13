09f60ef785d947cbbf0332cc8be1c1bfb24fd5ce
mmk4-81 <mmkkarbalaei@gmail.com>
2025-02-13 09:23:00 +0330

    add image generation functionality to create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,7 +24,31 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
 
- 
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -108,7 +132,7 @@ const CreatePost = () => {
         <div className="mt-5 flex gap-5">
           <button
             type="button"
-            // onClick=
+            onClick={generateImage}
             className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             {generatingImg ? 'Generating...' : 'Generate'}
